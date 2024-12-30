df2656480f0734347e53ddf6b1e352bba9773c59
Kevin Pacifico <k.pacifico@studenti.unisa.it>
2024-12-30 19:17:36 +0100

    update new project

--- fair-analytics/lib/share.js
+++
@@ -1,9 +0,0 @@
-const swarm = require('hyperdiscovery')
-
-module.exports = function share (feed) {
-  const sw = swarm(feed)
-  sw.on('connection', (peer, type) => {
-    console.log('connected to', sw.connections.length, 'peers')
-    peer.on('close', () => console.log('peer disconnected'))
-  })
-}
--- fair-analytics/test/sse.spec.js
+++
@@ -1,7 +0,0 @@
-const test = require('ava')
-const sse = require('../lib/sse')
-const SseChannel = require('sse-channel')
-
-test('exports a SseChannel instance', t => {
-  t.true(sse instanceof SseChannel)
-})
